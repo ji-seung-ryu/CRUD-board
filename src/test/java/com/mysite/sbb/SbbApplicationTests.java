3e89aff502503772c148366c70253f43244fdebb
ji-seung-ryu <8514199@gmail.com>
2022-06-04 17:15:23 +0900

    first commit

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.QuestionService;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionService questionService;
+	
+	@Test
+	void testJpa() {
+		for (int i=1;i<=300;i++) {
+			String subject = String.format("test data:[%03d]", i);
+			String content = "no";
+			this.questionService.create(subject, content);
+			
+		
+					
+		}
+	}
+    
+}
